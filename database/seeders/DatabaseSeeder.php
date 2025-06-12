bb04895464b3bde4b75b64c6834ecd853c57c494
CristianLeyton <leytoncristian96@gmail.com>
2025-06-12 18:48:05 -0300

    User seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -6,6 +6,7 @@ use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
+
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -16,8 +17,12 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'Admin',
+            'email' => 'admin@mail.com',
+           'password' => bcrypt('admin'),
         ]);
-    }
+
+     /*    $this->call(LocalidadesSeeder::class);
+        $this->call(NivelesDeFaltaSeeder::class); */
+}
 }
